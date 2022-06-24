5907c57bbe6431ecb855337969fbf3305e22ca07
EC2 Default User <ec2-user@ip-172-31-46-86.ap-northeast-1.compute.internal>
2022-06-24 17:50:53 +0000

    最終調節

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -42,9 +42,8 @@ Rails.application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
-
   # Print deprecation notices to the stderr.
-  config.active_support.deprecation = :stderr
+  config.active_support.deprecation = :silence
 
   # Raise exceptions for disallowed deprecations.
   config.active_support.disallowed_deprecation = :raise
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+   devise_for :users
+   resources :books
+   resources :users,only:[:show,:edit,:update]
+   root 'homes#top'
+   get 'users' => 'users#index'
+   get 'home/about' => 'layouts#about'
+   delete '/books' => 'books#destroy'
+
 end
